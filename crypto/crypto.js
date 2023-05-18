2e8fe21e9db7256448dbc0414b74cb650bec2faa
范晓豪 <fanxiaohaohaha@qq.com>
2023-05-18 11:08:27 +0800

    Buffer

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -1,17 +1,18 @@
-const crypto = require('crypto');
+const crypto = require('crypto')
 
-const iv = Buffer.from('0000000000000000', 'utf8'); // 初始化向量
-const algorithm = 'aes-256-cbc'; // 对称加密算法
-const key = crypto.randomBytes(32); // 生成一个密钥
+const iv = Buffer.from('0000000000000000', 'utf8') // 初始化向量
+const algorithm = 'aes-256-cbc' // 对称加密算法
+const key = crypto.randomBytes(32) // 生成一个密钥
 
-const cipher = crypto.createCipheriv(algorithm, key, iv);
-let encrypted = cipher.update('Hello, World!', 'utf8', 'hex');
-encrypted += cipher.final('hex');
-console.log(encrypted);
+const cipher = crypto.createCipheriv(algorithm, key, iv)
+
+let encrypted = cipher.update('Hello, World!', 'utf8', 'hex')
+encrypted += cipher.final('hex')
+console.log(encrypted)
 // 输出: 3e66d9ce536f2f2470a4c76a4653d9c8
 
-const decipher = crypto.createDecipheriv(algorithm, key, iv);
-let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-decrypted += decipher.final('utf8');
+const decipher = crypto.createDecipheriv(algorithm, key, iv)
+let decrypted = decipher.update(encrypted, 'hex', 'utf8')
+decrypted += decipher.final('utf8')
 console.log(decrypted);
 // 输出: Hello, World!
